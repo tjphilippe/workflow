a9338aa0b3a459e494800efe219ce2f041b784be
TJPHILIPPE <tjphilippe@ufl.edu>
2015-09-15 15:40:26 -0400

    Finish
    Coup out using text instead of images.

--- todo-src/script.js
+++ todo-src/script.js
@@ -6,24 +6,9 @@ myApp.controller('MainCtrl', function ($scope){
   $scope.todos = ["Learn Angular", "Learn node"];
   $scope.newItem = "";
   $scope.priority='';
-  
-  $scope.addItem = function(){
-    console.log("in add");
-    if ($scope.newItem !== ""){
-      $scope.todos.push($scope.newItem);
-      $scope.newItem = "";
-    }
-  }
-    
-  $scope.deleteItem = function(item){
-    console.log("in delete");
-    var index = $scope.todos.indexOf(item);
-    $scope.todos.splice(index, 1);
-    $scope.priorities.splice(index, 1);
-  }
 
-  $scope.priorities=["High","Low"];
-  $scope.prioritizeItem= function(){
+  $scope.priorities=["1","1"];
+  /*$scope.prioritizeItem= function(){
     console.log("in prioritize");
     if($scope.priority ==="1"){
       $scope.priorities.push($scope.priority);
@@ -40,7 +25,53 @@ myApp.controller('MainCtrl', function ($scope){
       $scope.priority = "";
     }
     $scope.priorities.toString();
+  }*/
+  
+  $scope.addItem = function(){
+    console.log("in add");
+    if ($scope.newItem !== ""){
+      $scope.todos.push($scope.newItem);
+      $scope.newItem = "";
+    }
+
+
+  $scope.priority = $('#select').val();
+    alert( $scope.priority);
+if($scope.priority==="Low"){
+  $scope.priority='Low Priority';
+}
+if($scope.priority !=="Low Priority" && $scope.priority !=="Normal Priority" && $scope.priority !=="High Priority" ){
+  $scope.priority='Low Priority';
+}
+      if ($scope.priority !== ""){
+ 
+    if($scope.priority ==="Low Priority"){
+       //$scope.priorities.push("<div style='display: inline; padding-right: 0.5em' class='pull-right'><button class='btn btn-info' style='background: black'><span class='glyphicon glyphicon-flag' aria-hidden='true'></span></button></div>");
+      $scope.priorities.push("Low Priority");
+      //$scope.priorities.push($scope.priority);
+      $scope.priority = "";
+     // $scope.priorities[index]="High";
+    }
+    else if($scope.priority==='Normal Priority'){
+      //$scope.priorities[index]="Med";
+         $scope.priorities.push($scope.priority);
+      $scope.priority = "";
+    } else if ($scope.priority==='High Priority'){
+      //$scope.priorities[index]="Low";
+      $scope.priorities.push("High Priority");
+      $scope.priority = "";
+    }
   }
+  }
+    
+  $scope.deleteItem = function(item){
+    console.log("in delete");
+    var index = $scope.todos.indexOf(item);
+    $scope.todos.splice(index, 1);
+    $scope.priorities.splice(index, 1);
+  }
+
+
     
 
   
